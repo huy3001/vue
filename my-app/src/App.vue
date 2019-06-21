5ed7f0c5a93a69536a0b1738289033c04df2f619
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2019-06-21 18:56:41 +0700

    Update Vue app and try create some components

---
+++ my-app/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="app">
+		<div class="container">
+			<img src="./assets/logo.png">
+			<h1>{{title}}</h1>
+			<p><b>{{fullName}}</b></p>
+			<FirstApp/>
+		</div>
+	</div>
+</template>
+
+<script>
+import FirstApp from './components/FirstApp';
+
+export default {
+	components: {
+		FirstApp
+	},
+	data: function() {
+		return {
+			title: 'This is Vue training',
+			firstName: 'Huy',
+			lastName: 'Nguyen'
+		}
+	},
+	computed: {
+		reverseTitle: function() {
+			return 'Reversed title is: ' + this.title.split('').reverse().join('')
+		},
+
+		fullName: function() {
+			return 'Developer is: ' + this.firstName + ' ' + this.lastName
+		}
+	},
+	created: function() {
+		console.log('Title is: ' + this.title)
+	},
+	name: 'App'
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
